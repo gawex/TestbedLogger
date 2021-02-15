4ac401faa14cc8648cfe0785c58e4806596fbcf5
gawex <lukas.tatarin@gmail.com>
2021-02-15 12:20:18 +0100

    LuTa: 1.00 - first commit for new release app version

--- app/src/main/java/cz/vsb/cbe/testbed/sql/Record.java
+++ app/src/main/java/cz/vsb/cbe/testbed/sql/Record.java
@@ -1,38 +1,92 @@
+/*
+  @author  Bc. Lukas Tatarin
+ * @supervisor Ing. Jaromir Konecny, Ph.D.
+ * @email   lukas@tatarin.cz
+ * @version 1.00
+ * @ide     Android Studio 4.1.2
+ * @license GNU GPL v3
+ * @brief   Record
+ * @lastmodify 2021/02/15 12:20:04
+ * @verbatim
+----------------------------------------------------------------------
+Copyright (C) Bc. Lukas Tatarin, 2021
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+<http://www.gnu.org/licenses/>
+ @endverbatim
+ */
+
 package cz.vsb.cbe.testbed.sql;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Date;
 
-public class Record{
-    private int DeId;
-    private float DaValue;
-    private Date DaTimeStamp;
+public class Record implements Comparable<Record> {
 
-    public Record(int deId, float daValue, long daTimeStamp){
-        DeId = deId;
-        DaValue = daValue;
-        DaTimeStamp = new Date(daTimeStamp);
+    private int mDaId;
+    private int mDeId;
+    private String mDaKey;
+    private Float mDaValue;
+    private Date mDaTimeStamp;
+
+    public Record() {
+        mDaValue = (float) 0;
     }
 
-    public Record(Record record){
-        DeId = record.getId();
-        DaValue = record.getValue();
-        DaTimeStamp = record.getTimeStamp();
+    public Record(int daId, int deId, String daKey, float daValue, long daTimeStamp) {
+        mDaId = daId;
+        mDeId = deId;
+        mDaKey = daKey;
+        mDaValue = daValue;
+        mDaTimeStamp = new Date(daTimeStamp);
     }
 
-    public int getId(){
-        return DeId;
+    @NotNull
+    @Override
+    public Record clone() {
+        try {
+            return (Record) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return new Record(mDaId, mDeId, mDaKey, mDaValue, mDaTimeStamp.getTime());
+        }
     }
 
-    public void setValue(float value){
-        DaValue = value;
+    public int getDataId() {
+        return mDaId;
+    }
+
+    public int getDeviceId() {
+        return mDeId;
+    }
+
+    public String getDataKey() {
+        return mDaKey;
     }
 
     public float getValue() {
-        return DaValue;
+        return mDaValue;
+    }
+
+    public void setValue(float value) {
+        mDaValue = value;
     }
 
     public Date getTimeStamp() {
-        return DaTimeStamp;
+        return mDaTimeStamp;
     }
 
+    @Override
+    public int compareTo(Record o) {
+        return mDaValue.compareTo(o.mDaValue);
+    }
 }
