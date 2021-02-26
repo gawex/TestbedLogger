13d3aba9cd63385c2c13ad31a578a0f20362091d
gawex <lukas.tatarin@gmail.com>
2021-02-26 12:42:29 +0100

    LuTa: 1.00 - initial commit

--- app/src/main/java/cz/vsb/cbe/testbed/SampleGattAttributes.java
+++ app/src/main/java/cz/vsb/cbe/testbed/SampleGattAttributes.java
@@ -1,57 +1,48 @@
+/*
+  @author  Bc. Lukas Tatarin
+ * @supervisor Ing. Jaromir Konecny, Ph.D.
+ * @email   lukas@tatarin.cz
+ * @version 1.00
+ * @ide     Android Studio 4.1.2
+ * @license GNU GPL v3
+ * @brief   SampleGattAttributes.java
+ * @lastmodify 2021/02/26 12:41:21
+ * @verbatim
+----------------------------------------------------------------------
+Copyright (C) Bc. Lukas Tatarin, 2021
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+<http://www.gnu.org/licenses/>
+ @endverbatim
+ */
+
 package cz.vsb.cbe.testbed;
 
-import java.util.HashMap;
 import java.util.UUID;
 
 public class SampleGattAttributes {
 
-    private static HashMap<UUID, String> ATTRIBUTES = new HashMap();
+    public static UUID TESTBED_SERVICE_UUID =
+            UUID.fromString("21110001-e780-4112-a997-5c3e475adbc3");
+    public static UUID TESTBED_ID_CHARACTERISTIC_UUID =
+            UUID.fromString("21110005-e780-4112-a997-5c3e475adbc3");
+    public static UUID HEART_RATE_CHARACTERISTIC_UUID =
+            UUID.fromString("21110003-e780-4112-a997-5c3e475adbc3");
+    public static UUID STEPS_CHARACTERISTIC_UUID =
+            UUID.fromString("21110004-e780-4112-a997-5c3e475adbc3");
+    public static UUID TEMPERATURE_CHARACTERISTIC_UUID =
+            UUID.fromString("21110002-e780-4112-a997-5c3e475adbc3");
 
-    public static UUID GENERIC_ACCESS_SERVICE_UUID              = UUID.fromString("00001800-0000-1000-8000-00805f9b34fb");
-    public static UUID DEVICE_NAME_CHARACTERISTIC_UUID          = UUID.fromString("00002a00-0000-1000-8000-00805f9b34fb");
-    public static UUID APPEARANCE_CHARACTERISTIC_UUID           = UUID.fromString("00002a01-0000-1000-8000-00805f9b34fb");
+    public static UUID CLIENT_CHARACTERISTIC_CONFIGURATION_UUID =
+            UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
 
-
-    public static UUID DEVICE_INFORMATION_SERVICE_UUID          = UUID.fromString("0000180a-0000-1000-8000-00805f9b34fb");
-    public static UUID MANUFACTURER_NAME_CHARACTERISTIC_UUID    = UUID.fromString("00002a29-0000-1000-8000-00805f9b34fb");
-
-    public static UUID TESTBED_SERVICE_UUID                     = UUID.fromString("21110001-e780-4112-a997-5c3e475adbc3");
-    public static UUID TESTBED_ID_CHARACTERISTIC_UUID           = UUID.fromString("21110005-e780-4112-a997-5c3e475adbc3");
-    public static UUID HEART_RATE_CHARACTERISTIC_UUID           = UUID.fromString("21110003-e780-4112-a997-5c3e475adbc3");
-    public static UUID STEPS_CHARACTERISTIC_UUID                = UUID.fromString("21110004-e780-4112-a997-5c3e475adbc3");
-    public static UUID TEMPERATURE_CHARACTERISTIC_UUID          = UUID.fromString("21110002-e780-4112-a997-5c3e475adbc3");
-
-    public static UUID CLIENT_CHARACTERISTIC_CONFIGURATION_UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
-    public static UUID CHARACTERISTIC_USER_DESCRIPTION_UUID     = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
-
-    public static String TESTBED_SERVICE_NAME                   = "Testbed";
-    public static String TESTBED_ID_CHARACTERISTIC_NAME         = "TestbedID";
-    public static String HEART_RATE_CHARACTERISTIC_NAME         = "Heart rate";
-    public static String STEPS_CHARACTERISTIC_NAME              = "Steps";
-    public static String TEMPERATURE_CHARACTERISTIC_NAME        = "Temperatrure";
-
-
-
-    static {
-        // Sample Services
-        ATTRIBUTES.put(GENERIC_ACCESS_SERVICE_UUID, "Generic Access");
-        ATTRIBUTES.put(DEVICE_INFORMATION_SERVICE_UUID, "Device Information");
-        ATTRIBUTES.put(TESTBED_SERVICE_UUID, TESTBED_SERVICE_NAME);
-        // Sample Characteristics
-        ATTRIBUTES.put(DEVICE_NAME_CHARACTERISTIC_UUID, "Device Name");
-        ATTRIBUTES.put(APPEARANCE_CHARACTERISTIC_UUID, "Appearance");
-        ATTRIBUTES.put(MANUFACTURER_NAME_CHARACTERISTIC_UUID, "Manufactured Name");
-        ATTRIBUTES.put(TEMPERATURE_CHARACTERISTIC_UUID, TEMPERATURE_CHARACTERISTIC_NAME);
-        ATTRIBUTES.put(HEART_RATE_CHARACTERISTIC_UUID, HEART_RATE_CHARACTERISTIC_NAME);
-        ATTRIBUTES.put(STEPS_CHARACTERISTIC_UUID, STEPS_CHARACTERISTIC_NAME);
-        ATTRIBUTES.put(TESTBED_ID_CHARACTERISTIC_UUID, TESTBED_ID_CHARACTERISTIC_NAME);
-    }
-
-    public static String lookupFromUUID(UUID uuid, String defaultName) {
-        String name = ATTRIBUTES.get(uuid);
-        if (name == null)
-            return defaultName;
-        else
-            return name;
-    }
 }
